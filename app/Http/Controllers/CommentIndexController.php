84f0271f7a429fac547febf1f7564603326bc6de
Rajesh Budhathoki <rajoyish@gmail.com>
2024-01-15 09:11:02 +0545

    05. Passing data to pages

--- app/Http/Controllers/CommentIndexController.php
+++ app/Http/Controllers/CommentIndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class CommentIndexController extends Controller
@@ -11,6 +12,8 @@ class CommentIndexController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return inertia()->render('Comments/Index');
+        return inertia()->render('Comments/Index', [
+            'posts' => Post::latest()->get(),
+        ]);
     }
 }
